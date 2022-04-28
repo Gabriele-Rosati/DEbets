510a86e62f353f81ec8cb4ad48f1a8a3236e8bd2
Gabriele Rosati <g.rosati00@outlook.it>
2022-04-28 14:17:17 +0200

    added tailwind and daisyUI

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,10 @@
 function Home() {
   return (
-    <div>Home</div>
+    <div class="card-normal bg-green-500">
+        <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
